2b102fc42e0cb03adbdb59557cc08f252f208015
James A Cone <jacone626@gmail.com>
2023-05-01 13:39:23 -0400

    added new-comment.js

--- public/js/new-comment.js
+++ public/js/new-comment.js
@@ -1,1 +1,29 @@
-//
+const newCommentFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const post_id = parseInt(window.location.pathname.split('/').pop());
+  
+    const content = document.querySelector('#new-comment-form').value.trim();
+  
+    if (content) {
+      const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({ content, post_id }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.reload(); 
+      } else {
+        alert('Failed to create a comment.');
+      }
+    }
+  };
+  
+  
+  
+  // Event listeners
+  const newCommentForm = document.querySelector('.new-comment-form');
+  if (newCommentForm) {
+    newCommentForm.addEventListener('submit', newCommentFormHandler);
+  }
